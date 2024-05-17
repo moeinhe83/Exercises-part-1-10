f9951eb1aa3b4116439c516e28fef0c4b1ea1afb
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-05-17 18:43:02 +0330

    Upload Exercises-part-1-10 Project On Github

---
+++ README.md
@@ -0,0 +1,25 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 10
+
+Programming Language ===> Python
+
+Program description ===> The Tenth exercise
+
+Example ===> Write A Program That Adds the chemistry Item To The Above table => lesson = (‘math’, ‘physics’)
+
+Description Of Files:
+
+practice_10_linux.py ===> For Linux [Ubuntu Or Kali]
+
+For Run ===> python practice_10_linux.py
+
+practice_10_windows.py ===> For Windows
+
+For Run ===> python practice_10_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ==> To install => pip install termcolor2
+
+pyfiglet ==> To install => pip install pyfiglet
---
+++ practice_10_windows.py
@@ -0,0 +1,27 @@
+# Practice_10
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('cls')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 10', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value
+lesson = ('math', 'physics')
+l = ('chemistry', )
+
+# Add item
+lesson = lesson+l
+print(lesson)
+
+# Finish
+# Create By Moein Heshmati
